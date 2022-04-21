545b609952bf6166429d8fd4e40518ae4acea502
s-woon <sus9763@gmail.com>
2022-04-21 16:53:53 +0900

    edit...

--- soccer_detecting.py
+++ soccer_detecting.py
@@ -12,27 +12,27 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox,
 from PyQt5.uic import loadUi
 from pytube import YouTube
 
-# from object_detection.utils import label_map_util
-# from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
-# sys.path.append("..")
-# PATH_TO_CKPT = './model/frozen_inference_graph.pb'
-# PATH_TO_LABELS = './data/mscoco_label_map.pbtxt'
-#
-# NUM_CLASSES = 90
-#
-# detection_graph = tf.compat.v1.Graph()
-# with detection_graph.as_default():
-#     od_graph_def = tf.compat.v1.GraphDef()
-#     with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-#         serialized_graph = fid.read()
-#         od_graph_def.ParseFromString(serialized_graph)
-#         tf.import_graph_def(od_graph_def, name='')
+sys.path.append("..")
+PATH_TO_CKPT = './model/frozen_inference_graph.pb'
+PATH_TO_LABELS = './data/mscoco_label_map.pbtxt'
 
-# label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-# categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-#                                                             use_display_name=True)
-# category_index = label_map_util.create_category_index(categories)
+NUM_CLASSES = 90
+
+detection_graph = tf.compat.v1.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
 save_dir = './video'
 
@@ -98,47 +98,6 @@ class WindowClass(QMainWindow):
     #         print(color)
     #         # print(color.name())
 
-
-    def count_nonblack_np(self, img):
-        """Return the number of pixels in img that are not black.
-        img must be a Numpy array with colour values along the last axis.
-
-        """
-        return img.any(axis=-1).sum()
-
-    def detect_team(self, image, show=False):
-        # define the list of boundaries
-        boundaries = [
-            ([17, 15, 100], [50, 56, 200]),  # red
-            ([25, 146, 190], [96, 174, 250])  # yellow
-        ]
-        i = 0
-        for (lower, upper) in boundaries:
-            # create NumPy arrays from the boundaries
-            lower = np.array(lower, dtype="uint8")
-            upper = np.array(upper, dtype="uint8")
-
-            # find the colors within the specified boundaries and apply
-            # the mask
-            mask = cv2.inRange(image, lower, upper)
-            output = cv2.bitwise_and(image, image, mask=mask)
-            tot_pix = self.count_nonblack_np(image)
-            color_pix = self.count_nonblack_np(output)
-            ratio = color_pix / tot_pix
-            #         print("ratio is:", ratio)
-            if ratio > 0.01 and i == 0:
-                return 'red'
-            elif ratio > 0.01 and i == 1:
-                return 'yellow'
-
-            i += 1
-
-            if show == True:
-                cv2.imshow("images", np.hstack([image, output]))
-                if cv2.waitKey(0) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-        return 'not_sure'
-
 # 비디오 재생관리
     def video(self, image):
         self.videoLb.setPixmap(QPixmap.fromImage(image).scaled(self.videoLb.size(), Qt.KeepAspectRatio))
@@ -149,13 +108,70 @@ class WindowClass(QMainWindow):
             upper = [50, 56, 200]
             lower = np.array(lower, dtype="uint8")
             upper = np.array(upper, dtype="uint8")
-        else:
-            lower = [25, 146, 190]
-            upper = [96, 174, 250]
+        elif color == 'Orange':
+            lower = [0, 100, 240]
+            upper = [120, 200, 255]
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+        elif color == 'Yellow':
+            lower = [0, 220, 210]
+            upper = [160, 255, 255]
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+        elif color == 'Green':
+            lower = [30, 100, 0]
+            upper = [200, 235, 200]
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+        elif color == 'Blue':
+            lower = [150, 100, 0]
+            upper = [255, 180, 150]
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+        elif color == 'Black':
+            lower = [0, 0, 0]
+            upper = [140, 140, 140]
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+        else: # Violet
+            lower = [150, 50, 100]
+            upper = [255, 180, 250]
             lower = np.array(lower, dtype="uint8")
             upper = np.array(upper, dtype="uint8")
 
         return lower, upper
+
+    def count_nonblack_np(self, img):
+        """Return the number of pixels in img that are not black.
+        img must be a Numpy array with colour values along the last axis.
+
+        """
+        return img.any(axis=-1).sum()
+
+    def detect_team(self, lower, upper, image, show=False):
+        # define the list of boundaries
+        i = 0
+
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+
+        # find the colors within the specified boundaries and apply
+        # the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+        tot_pix = self.count_nonblack_np(image)
+        color_pix = self.count_nonblack_np(output)
+        ratio = color_pix / tot_pix
+        #         print("ratio is:", ratio)
+        if ratio > 0.01 and i == 0:
+            return self.t1color
+        elif ratio > 0.01 and i == 1:
+            return self.t2color
+
+        if show == True:
+            cv2.imshow("images", np.hstack([image, output]))
+            if cv2.waitKey(0) & 0xFF == ord('q'):
+              cv2.destroyAllWindows()
 
     def detectstart(self):
         global source
@@ -171,16 +187,94 @@ class WindowClass(QMainWindow):
                 fps = 29.97
                 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
                 # writer = cv2.VideoWriter(source + '_detecting.avi', fourcc, fps, (640, 360))
-                t1name = self.t1namelb.text()
-                t2name = self.t2namelb.text()
-                t1color = str(self.t1colorbox.currentText())
-                t2color = str(self.t2colorbox.currentText())
+                self.t1name = self.t1namelb.text()
+                self.t2name = self.t2namelb.text()
+                self.t1color = self.t1colorbox.currentText()
+                self.t2color = self.t2colorbox.currentText()
+                print(self.t1name, self.t1color, self.t2name, self.t2color)
+                lower, upper = self.colorfinder(self.t1color)
+                print(lower, upper)
+                # t2color = self.colorfinder(t2color)
+                global cap
+                cap = cv2.VideoCapture(source)
 
-                t1color = self.colorfinder(t1color)
-                t2color = self.colorfinder(t2color)
+                with detection_graph.as_default():
+                    with tf.compat.v1.Session(graph=detection_graph) as sess:
+                        counter = 0
+                        while (True):
+                            ret, image_np = cap.read()
+                            counter += 1
+                            if ret:
+                                h = image_np.shape[0]
+                                w = image_np.shape[1]
 
-                print(t1color)
-                print(t2color)
+                            if not ret:
+                                break
+                            if counter % 1 == 0:
+                                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                                image_np_expanded = np.expand_dims(image_np, axis=0)
+                                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                                # Each box represents a part of the image where a particular object was detected.
+                                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                                # Each score represent how level of confidence for each of the objects.
+                                # Score is shown on the result image, together with the class label.
+                                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                                # Actual detection.
+                                (boxes, scores, classes, num_detections) = sess.run(
+                                    [boxes, scores, classes, num_detections],
+                                    feed_dict={image_tensor: image_np_expanded})
+                                # Visualization of the results of a detection.
+                                vis_util.visualize_boxes_and_labels_on_image_array(
+                                    image_np,
+                                    np.squeeze(boxes),
+                                    np.squeeze(classes).astype(np.int32),
+                                    np.squeeze(scores),
+                                    category_index,
+                                    use_normalized_coordinates=True,
+                                    line_thickness=3,
+                                    min_score_thresh=0.6)
+
+                                frame_number = counter
+                                loc = {}
+                                for n in range(len(scores[0])):
+                                    if scores[0][n] > 0.60:
+                                        # Calculate position
+                                        ymin = int(boxes[0][n][0] * h)
+                                        xmin = int(boxes[0][n][1] * w)
+                                        ymax = int(boxes[0][n][2] * h)
+                                        xmax = int(boxes[0][n][3] * w)
+
+                                        # Find label corresponding to that class
+                                        for cat in categories:
+                                            if cat['id'] == classes[0][n]:
+                                                label = cat['name']
+
+                                        ## extract every person
+                                        if label == 'person':
+                                            # crop them
+                                            crop_img = image_np[ymin:ymax, xmin:xmax]
+                                            color = self.detect_team(lower, upper, crop_img)
+                                            if color != 'not_sure':
+                                                coords = (xmin, ymin)
+                                                if color == self.t1color:
+                                                    loc[coords] = self.t1name
+                                                else:
+                                                    loc[coords] = self.t2name
+
+                                ## print color next to the person
+                                for key in loc.keys():
+                                    text_pos = str(loc[key])
+                                    cv2.putText(image_np, text_pos, (key[0], key[1] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                                                0.50, (255, 0, 0), 2)  # Text in black
+
+                            cv2.imshow('image', image_np)
+
+                            if cv2.waitKey(10) & 0xFF == ord('q'):
+                                cv2.destroyAllWindows()
+                                cap.release()
+                                break
 
 
                 # vid = cv2.VideoCapture(source)
